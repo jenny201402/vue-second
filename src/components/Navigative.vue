f33015f64ec5c8fe284cb07ed2c92d434f0e351f
jenny <chiely2018@gmail.com>
2019-06-01 20:00:58 +0800

    crossArray

--- src/components/Navigative.vue
+++ src/components/Navigative.vue
@@ -1,13 +1,42 @@
 <template>
   <div url="/profile">
-    <div>navigate</div>
-    <div>navigate</div>
+    <div>
+        <div v-for="name in country"></div>
+        <div v-for="put in putInside"></div>
+    </div>
   </div>
 </template>
 
 <script>
     export default {
-        name: "Navigative"
+        name: "Navigative",
+      data(){
+          return{
+            country: [
+                {name:'china'},
+                {name:'england'},
+                {name:'america'},
+                {name:'korean'},
+                {name:'japanese'},
+                {name:'arilan'},
+                {name:'arben'},
+                {name:'alphalan'},
+              ],
+            putInside: [
+              {put:'1'},
+              {put:'2'},
+              {put:'3'},
+              {put:'4'},
+              {put:'5'},
+              {put:'6'},
+              {put:'7'},
+            ]
+          }
+      },
+      methods: {},
+      mounted(){
+          console.log()
+      },
     }
 </script>
 
